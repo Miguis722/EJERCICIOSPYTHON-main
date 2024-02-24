70704509edec6e9b39f367383dabaa229967b5ff
Miguis722 <160697097+Miguis722@users.noreply.github.com>
2024-02-24 14:43:09 -0500

    Desarrollo del ejercicio numero tres

---
+++ Ejercicio3.py
@@ -0,0 +1,8 @@
+Saludo = ("Bienvenido al exponenciador")
+print(Saludo)
+
+num = int(input("Por favor, introduzca el número: "))
+
+resultado = (num ** num)
+
+print("El resultado de la exponenciación es de: ", resultado)
