9e8dc492c0e54a391597b83f9c51429032665265
Miguis722 <160697097+Miguis722@users.noreply.github.com>
2024-02-24 13:09:58 -0500

    Desarrollo del ejercicio numero diez

---
+++ Ejercicio10.py
@@ -0,0 +1,10 @@
+Saludo = ("Bienvenido al calculador de área de un círculo")
+print(Saludo)
+
+Radio = float(input("Por favor, introduzca el valor del radio en valor de centimetros: "))
+
+Radioala2 = Radio * Radio
+
+Resultado = float(Radioala2) * 3.14 
+
+print("El área del circulo es de: ", Resultado, "cm")
