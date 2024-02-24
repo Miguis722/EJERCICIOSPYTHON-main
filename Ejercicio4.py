66fdd9258b879426dc3991cf92ce96054717aaed
Miguis722 <160697097+Miguis722@users.noreply.github.com>
2024-02-24 18:29:45 -0500

    Desarrollo del ejercicio numero 4

---
+++ Ejercicio4.py
@@ -0,0 +1,12 @@
+Saludo = ("Bienvenido al determinador de números primos")
+print(Saludo)
+
+numero = float(input("Por favor, digite el número: "))
+
+resultado = (numero / 2)
+
+if resultado == 1:
+    print("El número es primo")
+if resultado == 0:
+    print("El número no es primo")
+    
