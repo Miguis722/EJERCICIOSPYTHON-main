7f018bd110d162eb41980940dd289b6beb1f422c
Miguis722 <160697097+Miguis722@users.noreply.github.com>
2024-02-24 14:37:54 -0500

    Desarrollo del ejercicio numero cuatro Nota: Mirar los operador logicos de PYTHON

---
+++ Ejercicio4.py
@@ -0,0 +1,14 @@
+Saludo = ("Bienvenido al determinador de pares e impares")
+print(Saludo)
+
+num_1 = int(input("Por favor, introduzca el número: "))
+
+resultado = (num_1) % (2)
+
+
+
+if resultado is 0:
+ print("El número es par")
+elif resultado is 1 or num_1: 
+ print("El número es impar")
+
