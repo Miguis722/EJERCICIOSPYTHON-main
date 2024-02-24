159d4749ff3975bf6c89cd601ccb7bab67ce1f5f
Miguis722 <160697097+Miguis722@users.noreply.github.com>
2024-02-24 13:18:20 -0500

    Desarrollo del ejercicio numero seis

---
+++ Ejercicio6.py
@@ -0,0 +1,14 @@
+Saludo = ("Bienvenido al calculador de área de un triángulo")
+print(Saludo)
+
+Base = float(input("Por favor, introduzca el valor de la base del triangulo: "))
+
+Mitad = Base / 2
+
+Altura = float(input("Por favor, introduzca el vaor de la altura del triangulo: "))
+
+Área = Mitad * Altura
+
+print("El Área del triangulo es de: ", Área)
+
+
