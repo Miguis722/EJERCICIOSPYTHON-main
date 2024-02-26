442b4bd95b5a069ef79a108ee7dbfb8f08afc222
Miguis722 <160697097+Miguis722@users.noreply.github.com>
2024-02-26 07:31:40 -0500

    Corrección del desarrollo del ejercicio

--- Ejercicio3-FACTORIAL_DE_UN_NUMERO.py
+++ Ejercicio3-FACTORIAL_DE_UN_NUMERO.py
@@ -1,8 +1,12 @@
 Saludo = ("Bienvenido al exponenciador")
 print(Saludo)
 
-num = int(input("Por favor, introduzca el número: "))
-
-resultado = (num ** num)
+def factorial (numero):
+  if numero == 0:
+    return 1
+  else:
+    return numero * factorial(numero - 1)
+    numero = int(input("Por favor, escriba el nùmero a factorizar: "))
+    resultado = factorial (numero)
 
 print("El resultado de la factorializaciòn es de: ", resultado)
