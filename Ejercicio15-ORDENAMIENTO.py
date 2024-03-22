112442dbc27b9543e9b7443e3b680a49e789bc66
Miguis722 <160697097+Miguis722@users.noreply.github.com>
2024-03-21 22:35:59 -0500

    Desarrollo del ejercicio 15

---
+++ Ejercicio15-ORDENAMIENTO.py
@@ -0,0 +1,7 @@
+numero1 = int(input("Ingrese el primer numero: "))
+numero2 = int(input("Ingrese el segundo numero: "))
+numero3 = int(input("Ingrese el tercer numero: "))
+numero4 = int(input("Ingrese el cuarto numero: "))
+
+numero_ordenado = sorted([numero1,numero2,numero3,numero4])
+print("En orrden de menor a mayor:",numero_ordenado)
